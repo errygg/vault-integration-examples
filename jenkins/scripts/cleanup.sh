cbf11a336b922f935030cb962550374fd693ac97
Erik R. Rygg <errygg@gmail.com>
2020-09-23 14:13:32 -0600

    Updating the jenkins stuff to work

--- jenkins/scripts/cleanup.sh
+++ jenkins/scripts/cleanup.sh
@@ -1,10 +1,9 @@
 # Stop all the processes
 pkill vault
-#lsof -i TCP:8080 | awk '/LISTEN/ {print $2}' | xargs kill -9
 sleep 5 # Wait for the sockets to close
 
 # Remove the log files
-rm -rf /tmp/vault-output.txt /tmp/vault.log /tmp/roleid /tmp/secretid /tmp/vault-agent-token
+rm -rf /tmp/vault-output.txt /tmp/vault.log /tmp/roleid /tmp/secretid /tmp/vault-agent-token /tmp/vault-agent.log
 
 # Remove all the data dirs
 rm -rf /tmp/vault
